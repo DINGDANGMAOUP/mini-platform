6e19481452d2f497031b112023748838aa19d347
dingdangmaoup <dingdangmaoup@gmail.com>
2023-04-26 16:47:10 +0800

    feat(confi): add
    1,添加lomobok config

--- mini-adapter/src/main/java/com/dingdangmaoup/mini/platform/adapter/web/TestController.java
+++ mini-adapter/src/main/java/com/dingdangmaoup/mini/platform/adapter/web/TestController.java
@@ -1,13 +1,13 @@
 package com.dingdangmaoup.mini.platform.adapter.web;
 
+import com.alibaba.cola.dto.SingleResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
   @RequestMapping("/test")
-  public String test() {
-
-    return "test";
+  public SingleResponse<String> test() {
+    return SingleResponse.of("Hello World");
   }
 }
